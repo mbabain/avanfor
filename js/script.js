c85f47af06d9cdfbd858798f1e35e490c38d38f7
Maksim Babain <mababain@mail.ru>
2020-10-08 22:39:02 +0700

    second day commit

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
         infinite: true,
         variableWidth: true
     });
+    $('#blog-slider').slick({
+        slidesToShow: 3,
+        speed: 200,
+        infinite: true,
+        variableWidth: true
+    });
+    $('#our-works-slider').slick({
+        slidesToShow: 4,
+        speed: 200,
+        infinite: true,
+        variableWidth: true
+    });
 });
