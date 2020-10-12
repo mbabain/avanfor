ecea97ef8e747623583c20fdcd0d8745856fd2e8
Maksim Babain <mababain@mail.ru>
2020-10-12 18:08:34 +0700

    third day1

--- js/product.js
+++ js/product.js
@@ -10,3 +10,21 @@ minus.addEventListener('click', () => {
 plus.addEventListener('click', () => {
         document.querySelector('.in-num').value++;
 });
+
+document.querySelectorAll('.section-product-tabs-name').forEach(el => {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = e.target.getAttribute('href').replace('#', '');
+
+        document.querySelectorAll('.section-product-tabs-name').forEach(
+            child => child.classList.remove('section-product-tabs-name--active'));
+        document.querySelectorAll('.section-product-tabs-text').forEach(
+            child => child.classList.remove('section-product-tabs-text--active'));
+            
+            el.classList.add('section-product-tabs-name--active');
+            document.getElementById(id).classList.add('section-product-tabs-text--active');
+    });
+
+});
+
+document.querySelector('.section-product-tabs-name').click();
